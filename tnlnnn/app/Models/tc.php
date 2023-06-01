281d6291ce6580b473e61a8355af0ef1d039fc60
sijot <mikioo@gmail.com>
2023-06-01 11:53:44 +0700

    upload thu muc tnlnnn vo github

---
+++ tnlnnn/app/Models/tc.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tc extends Model
+{
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+}
